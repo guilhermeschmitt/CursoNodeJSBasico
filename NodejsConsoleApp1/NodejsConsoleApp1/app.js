39bc00b9dcef40dc09529085d5e4c2dc6f26cab7
Guilherme <Guilherme@T800>
2017-06-19 14:57:35 -0300

    Teste de commit do primeiro desafio da aula um

---
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -0,0 +1,4 @@
+var carros = require('./data/carros');
+var percorreListaDeCarros = require('./service/carrosService');
+
+percorreListaDeCarros(carros);
---
+++ NodejsConsoleApp1/NodejsConsoleApp1/service/carrosService.js
@@ -0,0 +1,11 @@
+module.exports = function percorreListaDeCarros(array) {
+    array.forEach(function (carro) {
+        console.log("As informações dos carros são: carro" + i +
+            "\n marca: " + carro.marca + "," +
+            "\n modelo: " + carro.modelo + "," +
+            "\n ano: " + carro.ano + "," +
+            "\n cor: " + carro.cor + "," +
+            "\n versao: " + carro.versao + "," +
+            "\n preço: " + carro.preco + ", \n");
+    });
+}
